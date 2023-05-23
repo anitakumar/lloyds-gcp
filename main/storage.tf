c81f4cdd38b6ee5bd566a537b5dc9537686cd02e
Anita Kumar <anitakumar>
2023-05-23 23:52:27 +0100

    Update storage configuration

--- main/storage.tf
+++ main/storage.tf
@@ -1,8 +1,8 @@
 resource "google_storage_bucket" "this" {
-  provider = google-beta
-  name     = "${var.name}-${var.env}-bucket"
-  storage_class = "NEARLINE"
-  location = var.region
+  provider                 = google-beta
+  name                     = "${var.name}-${var.env}-bucket"
+  storage_class            = "NEARLINE"
+  location                 = var.region
   public_access_prevention = "enforced"
   retention_policy {
     # is_locked = true #do not use you cannot change the retention period
@@ -12,20 +12,21 @@ resource "google_storage_bucket" "this" {
     condition {
       age = 5
     }
-  action {
-    type = "SetStorageClass"
-    storage_class ="COLDLINE"
-  }
+    action {
+      type          = "SetStorageClass"
+      storage_class = "COLDLINE"
+    }
 
   }
   labels = {
     "env" = "dev"
   }
   uniform_bucket_level_access = true
+  force_destroy               = true
 
 }
-resource "google_storage_bucket_object" "this"{
-  name = "${var.name}-${var.env}-${var.filename}"
-  bucket =  google_storage_bucket.this.name
+resource "google_storage_bucket_object" "this" {
+  name   = "${var.name}-${var.env}-${var.filename}"
+  bucket = google_storage_bucket.this.name
   source = "${path.module}/${var.filename}"
 }
